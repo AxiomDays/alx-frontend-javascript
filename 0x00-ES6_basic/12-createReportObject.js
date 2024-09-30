da84e35bc033479cfc8da42775254d68adfac331
AxiomDays <tobeoke@gmail.com>
2024-09-30 18:40:52 +0100

    task12

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,9 @@
+export default function createReportObject(employeesList) {
+	return{
+		["allEmployees"]: employeesList,
+
+		getNumberOfDepartments(x){
+			return Object.keys(x).length;
+		}
+	}
+}
