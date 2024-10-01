21ead5a46ef47180252beb8046dd4b65a47d52d7
AxiomDays <tobeoke@gmail.com>
2024-10-01 10:46:41 +0100

    test5v2

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,3 +1,4 @@
 export default function concatArrays(array1, array2, string) {
-	return (array1.concat(...array2).concat(...string));
+	const temp = [];
+	return (temp.concat(...array1).concat(...array2).concat(...string));
 }
